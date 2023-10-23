7dbf70859bc542d686ecc9383afadc68e0de7a87
hannahqmartin <104391834+hannahqmartin@users.noreply.github.com>
2023-10-23 15:57:33 -0400

    updated the hospital taxonomy

--- hospitals/hospitals.R
+++ hospitals/hospitals.R
@@ -59,7 +59,7 @@ bmf$hosp[ bmf$F9_04_HOSPITAL_X == 1 | bmf$F9_04_HOSPITAL_X == "true" ] <- 1
 
 table( bmf$hosp )
 
-# Create variables
+# Create hospital taxonomy
 bmf$h <- rep( 16, nrow(bmf) )
 bmf$h[ !( bmf$NTEEFINAL %in% c( "E20", "E22",
                                 "E21", "E32", "E30", "E31", "E80",
@@ -115,6 +115,115 @@ bmf$h[ bmf$NTEEFINAL %in% c( "G20", "G25", "G30", "G32", "G40", "G41", "G42",
                              "G43", "G44", "G45", "G48", "G50", "G51", "G54", 
                              "G60", "G61", "G70", "G80", "G81", "G83", "G84", 
                              "G99" )] <- 15
+
+table( bmf$h )
+table( bmf$h, bmf$hosp )
+
+# Investigate the hospitals that are in the
+# "not a hospital, health/feacility/service provider, or
+# health infrastructure/ service provider" category
+d_0_hosp <- bmf[ which( bmf$h == 0 & bmf$hosp == 1 ), ]
+write.csv( d_0_hosp, "d_0_hosp.csv" )
+
+# Recode the problematic NTEEs
+bmf$NTEEFINAL[ bmf$EIN %in% c( 42794625, 203700131, 232201344, 250965292, 
+                               251925187, 260335902, 261720984, 261947374, 
+                               263734606, 264230898, 273733278, 275401105,
+                               311518658, 331065485, 340714756, 380350304, 
+                               381676780, 383833117, 450227753, 463851119, 
+                               473793197, 474196634, 476028103, 480958974, 
+                               481226976, 481226977, 593051173, 640655993, 
+                               741287016, 742759890, 811708177, 812240617, 
+                               814653630, 822567308, 822748395, 824372339, 
+                               841276376 )] <- "E20"
+bmf$NTEEFINAL[ bmf$EIN %in% c( 452964467, 455023260, 581509251, 630931008,
+                               821406555 )] <- "E21"
+bmf$NTEEFINAL[ bmf$EIN %in% c( 760536234, 411801967 )] <- "E32"
+bmf$NTEEFINAL[ bmf$EIN %in% c( 370808925, 520607921 )] <- "E91"
+bmf$NTEEFINAL[ bmf$EIN %in% c( 880401489 )] <- "F22"
+bmf$NTEEFINAL[ bmf$EIN %in% c( 990298651 )] <- "F30"
+
+# Add an "other hospitals" category to the hospital taxonomy
+bmf$h <- rep( 17, nrow(bmf) )
+bmf$h[ !( bmf$hosp == 1 & bmf$NTEEFINAL %in% c( "E20", "E22",
+                                "E21", "E32", "E30", "E31", "E80",
+                                "E24", "E61", "E65", "E6A",
+                                "E60", "E50", "E99", "P50", "E86",
+                                "F40", "E62", "P52", "P60", "P61", "P58", "P85", 
+                                "P84",
+                                "P81", "P75", "E90", "E91", "P74", "E92",
+                                "P70", "P73", "P71", "P80", "P82", "P86", 
+                                "P87",
+                                "E40", "E42", "P47", "P83", "P88",
+                                "F42", "P43", "P62",
+                                "P40", "P42", "P44", "P45", "P46",
+                                "F31", "F33", "F32", "F30", "F60", "F70", 
+                                "F99",
+                                "F22", "F20", "F53", "F21", "F50", "F52", "F54", 
+                                "P51",
+                                "E05", "F05", "G05", "H05", "H20", "H25", "H30", 
+                                "H32", "H40", "H41", "H42", "H43", "H44", "H45", 
+                                "H48", "H50", "H51", "H54", "H60", "H61", "H70", 
+                                "H80", "H81", "H83", "H84", "H90", "H92", "H94", 
+                                "H96", "H98", "H9B", "H99",
+                                "U30",
+                                "G20", "G25", "G30", "G32", "G40", "G41", "G42", 
+                                "G43", "G44", "G45", "G48", "G50", "G51", "G54", 
+                                "G60", "G61", "G70", "G80", "G81", "G83", "G84", 
+                                "G99" ))] <- 0
+bmf$h[ bmf$NTEEFINAL %in% c( "E20", "E22" ) | ( bmf$NTEEFINAL == "B43" & 
+                                                  bmf$hosp == 1 )] <- 1
+bmf$h[ bmf$NTEEFINAL %in% c( "E21", "E32", "E30", "E31", "E80" )] <- 2
+bmf$h[ bmf$NTEEFINAL %in% c( "E24", "E61", "E65", "E6A" )] <- 3
+bmf$h[ bmf$NTEEFINAL %in% c( "E60", "E50", "E99", "P50", "E86" )] <- 4
+bmf$h[ bmf$NTEEFINAL %in% c( "F40", "E62", "P52", "P60", "P61", "P58", "P85", 
+                             "P84" )] <- 5
+bmf$h[ bmf$NTEEFINAL %in% c( "P81", "P75", "E90", "E91", "P74", "E92" )] <- 6
+bmf$h[ bmf$NTEEFINAL %in% c( "P70", "P73", "P71", "P80", "P82", "P86", 
+                             "P87" )] <- 7
+bmf$h[ bmf$NTEEFINAL %in% c( "E40", "E42", "P47", "P83", "P88" )] <- 8
+bmf$h[ bmf$NTEEFINAL %in% c( "F42", "P43", "P62" )] <- 9
+bmf$h[ bmf$NTEEFINAL %in% c( "P40", "P42", "P44", "P45", "P46" )] <- 10
+bmf$h[ bmf$NTEEFINAL %in% c( "F31", "F33", "F32", "F30", "F60", "F70", 
+                             "F99" )] <- 11
+bmf$h[ bmf$NTEEFINAL %in% c( "F22", "F20", "F53", "F21", "F50", "F52", "F54", 
+                             "P51" )] <- 12
+bmf$h[ bmf$NTEEFINAL %in% c( "E05", "F05", "G05", "H05", "H20", "H25", "H30", 
+                             "H32", "H40", "H41", "H42", "H43", "H44", "H45", 
+                             "H48", "H50", "H51", "H54", "H60", "H61", "H70", 
+                             "H80", "H81", "H83", "H84", "H90", "H92", "H94", 
+                             "H96", "H98", "H9B", "H99" )] <- 13
+bmf$h[ bmf$NTEEFINAL %in% c( "U30" )] <- 14
+bmf$h[ bmf$NTEEFINAL %in% c( "G20", "G25", "G30", "G32", "G40", "G41", "G42", 
+                             "G43", "G44", "G45", "G48", "G50", "G51", "G54", 
+                             "G60", "G61", "G70", "G80", "G81", "G83", "G84", 
+                             "G99" )] <- 15
+bmf$h[ bmf$hosp == 1 & !( bmf$NTEEFINAL %in% c( "E20", "E22", "B43",
+                                "E21", "E32", "E30", "E31", "E80",
+                                "E24", "E61", "E65", "E6A",
+                                "E60", "E50", "E99", "P50", "E86",
+                                "F40", "E62", "P52", "P60", "P61", "P58", "P85", 
+                                "P84",
+                                "P81", "P75", "E90", "E91", "P74", "E92",
+                                "P70", "P73", "P71", "P80", "P82", "P86", 
+                                "P87",
+                                "E40", "E42", "P47", "P83", "P88",
+                                "F42", "P43", "P62",
+                                "P40", "P42", "P44", "P45", "P46",
+                                "F31", "F33", "F32", "F30", "F60", "F70", 
+                                "F99",
+                                "F22", "F20", "F53", "F21", "F50", "F52", "F54", 
+                                "P51",
+                                "E05", "F05", "G05", "H05", "H20", "H25", "H30", 
+                                "H32", "H40", "H41", "H42", "H43", "H44", "H45", 
+                                "H48", "H50", "H51", "H54", "H60", "H61", "H70", 
+                                "H80", "H81", "H83", "H84", "H90", "H92", "H94", 
+                                "H96", "H98", "H9B", "H99",
+                                "U30",
+                                "G20", "G25", "G30", "G32", "G40", "G41", "G42", 
+                                "G43", "G44", "G45", "G48", "G50", "G51", "G54", 
+                                "G60", "G61", "G70", "G80", "G81", "G83", "G84", 
+                                "G99" ))] <- 16
 
 table( bmf$h )
 table( bmf$h, bmf$hosp )
