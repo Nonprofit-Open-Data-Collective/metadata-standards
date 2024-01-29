337b5900b10083f6de8acc3016c6cd835d95a371
Olivia Beck <olb5053@psu.edu>
2024-01-29 11:28:05 -0500

    updating scores to use polychoric correlations

--- governance/pkg-funcs/storing-factor-objects.R
+++ governance/pkg-funcs/storing-factor-objects.R
@@ -10,26 +10,29 @@ library(psych)
 load("governance/governance-design-matrix.Rdata")
 
 ### Make Model ---------------------------------
+# use polychoric correlations
 model2.6 = fa(features2, nfactor=6, cor="poly", fm="mle", rotate = "equamax", 
               scores = "Thurstone")
 
-### Store Necessary object from model --------------------------
-weights <- model2.6$weights
-scores <- model2.6$scores
+### Calculating Scores -------------------------------------------------
+# do not rely on output from fa.model object. 
+# fa.model object uses the incorrect correlation matrix. 
+# You must specify using the polychoric correlations to calculate the scores 
+# Use "Thurstone" for scores method to get regression calculations
+scores <- factor.scores(features2, 
+                        model2.6, 
+                        rho = polychoric(features2)$rho,
+                        method = "Thurstone")
 
-### Get scaling factors (were used in calculating weights, needed to scale new data)
-data.scaled <- scale(features2)
-scaled.mean <- attr(data.scaled, "scaled:center")
-scaled.sd <- attr(data.scaled, "scaled:scale")
+# add scores + total score to feature2 matrix 
+features2 <- as.data.frame(cbind(features2, scores$scores))
+features2$total.score <- rowSums(features2[ ,c(paste0("ML", 1:6))]) 
 
 ### Saving these objects to be used in governance score
 save.image(file = "governance/pkg-funcs/factor-objects.Rdata")
 
 ### Objects in this  R data
-# features2 - design matrix 
-# model2.6 - EFA model with 6 factors 
-# weights - weights from model2.6
-# scores - scores from model2.6
-# data.scaled = features2 but columns are scaled to have 0 mean and 1 sd
-# scaled.mean = column means of features2
-# scaled.sd = column sd of features2
+# features2 = design matrix with scores 
+# features2_ID = ID/EIN/AWS information of each entry used in the features2 matrix, matches on row. 
+# model2.6 = EFA model with 6 factors
+# scores = scores from model2.6 using polychoric correlations
